ce39dcc5be4e2d21e460ba8b3b807e71c45efff5
MuhammadRauf07 <raufdev202@gmail.com>
2024-09-12 16:04:22 +0500

    add readme and licens

--- README.md
+++ README.md
@@ -1,22 +1,76 @@
+
 # ProKitSnackBar
 
-ProKitSnackBar is a customizable and dynamic Flutter widget for displaying various types of snack bars with rich features, including positioning, auto-close functionality, custom icons, and notification types.
+`ProKitSnackBar` is a highly customizable Flutter widget designed for displaying snack bars with advanced features. It offers extensive customization options, including positioning, auto-closing functionality, diverse notification types, and flexible design elements, making it suitable for both mobile and web platforms.
 
 ## Features
 
-- **Customizable Positioning:** Choose where the SnackBar appears on the screen, such as top, center, or bottom.
-- **Notification Types:** Provide different visual feedback like success, failure, warning, and help notifications.
-- **Auto-Close:** Automatically dismiss the SnackBar after a certain duration.
-- **Flexible Design:** Customize the SnackBar’s width, height, colors, text styles, and icons.
-- **Cross-Platform Support:** Works seamlessly on mobile and web platforms.
+- **Customizable Positioning:** Control the SnackBar's position on the screen with options for top, center, or bottom placement.
+- **Notification Types:** Display various types of notifications including success, error, warning, and information, each with distinct visual styles.
+- **Auto-Close Functionality:** Set a duration for the SnackBar to automatically dismiss itself, or configure it to remain visible until manually closed.
+- **Flexible Design:** Tailor the SnackBar’s appearance with customizable width, height, background colors, text styles, and icons.
+- **Cross-Platform Support:** Ensures consistent performance and appearance on both mobile and web platforms.
+
+## Properties
+
+- **`position`**: `SnackBarPosition` - Defines the position of the SnackBar on the screen. Options include `top`, `center`, and `bottom`.
+- **`type`**: `SnackBarType` - Specifies the notification type. Options include `success`, `error`, `warning`, and `info`.
+- **`autoCloseDuration`**: `Duration` - Duration before the SnackBar automatically closes. Set to `null` for no auto-close.
+- **`backgroundColor`**: `Color` - Custom background color for the SnackBar.
+- **`textStyle`**: `TextStyle` - Style configuration for the SnackBar's text.
+- **`icon`**: `Widget` - Custom icon displayed alongside the SnackBar message.
+- **`width`**: `double` - Width of the SnackBar. Set to `double.infinity` for full width.
+- **`height`**: `double` - Height of the SnackBar.
+- **`action`**: `SnackBarAction` - Optional action button with a callback.
+
+## Getting Started
+
+To use `ProKitSnackBar` in your Flutter project, add it to your `pubspec.yaml` file:
+
+```yaml
+dependencies:
+  prokit_snackbar: ^latest_version
+```
+
+## Usage
+
+Here’s a simple example of how to use `ProKitSnackBar`:
+
+```dart
+import 'package:flutter/material.dart';
+import 'package:prokit_snackbar/prokit_snackbar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('ProKitSnackBar Example')),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              ProKitSnackBar.show(
+                context,
+                message: 'This is a success message!',
+                type: SnackBarType.success,
+                autoCloseDuration: Duration(seconds: 3),
+              );
+            },
+            child: Text('Show SnackBar'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+```
+
+## Screenshots
 
 ![Success SnackBar Example](screenshots/success.png)
 ![Error SnackBar Example](screenshots/error.png)
 
-## Getting started
-
-To use ProKitSnackBar in your Flutter project, add it to your `pubspec.yaml` file:
-
-```yaml
-dependencies:
-  prokit_snackbar: latest_version
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:example/snackbar_demo.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart';
+import 'package:pro_kit_snackbar/snackBar/snack_bar.dart';
+import 'package:pro_kit_snackbar/snackBar/snackbar_enum.dart';
 
 void main() => runApp(
-  DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => MyApp(), // Wrap your app
-  ),
+  MyApp(),
 );
 
 class MyApp extends StatelessWidget {
@@ -15,11 +11,258 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       home: const SnackBarDemo(),
+    );
+  }
+}
+
+class SnackBarDemo extends StatelessWidget {
+  const SnackBarDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ProKit SnackBar Grid Example'),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.count(
+            crossAxisCount: MediaQuery.of(context).size.width > 600 ? 3 : 2,
+            // 3 columns for larger screens, 2 for smaller
+            childAspectRatio:
+            MediaQuery.of(context).size.width > 600 ? 4 : 2.5,
+            // Adjusts ratio for better text visibility
+            crossAxisSpacing: 8.0,
+            // Spacing between columns
+            mainAxisSpacing: 8.0,
+            // Spacing between rows
+            children: _buildSnackBarGridItems(context),
+          )),
+    );
+  }
+
+  /// Builds the list of Grid Items for displaying SnackBars
+  List<Widget> _buildSnackBarGridItems(BuildContext context) {
+    return [
+      _gridSnackBarItem(
+        context: context,
+        label: "Show Bordered Success SnackBar",
+        onPressed: () => _showSuccessSnackBar(context),
+      ),
+      _gridSnackBarItem(
+        context: context,
+        label: "Show Bordered Error SnackBar",
+        onPressed: () => _showErrorSnackBar(context),
+      ),
+      _gridSnackBarItem(
+        context: context,
+        label: "Show Bordered Warning SnackBar",
+        onPressed: () => _showWarningSnackBar(context),
+      ),
+      _gridSnackBarItem(
+        context: context,
+        label: "Show Bordered Help SnackBar",
+        onPressed: () => _showHelpSnackBar(context),
+      ),
+      _gridSnackBarItem(
+        context: context,
+        label: "Show Bordered Custom Size SnackBar",
+        onPressed: () => _showCustomSizeSnackBar(context),
+      ),
+      _gridSnackBarItem(
+        context: context,
+        label: "Show Bordered Centered SnackBar",
+        onPressed: () => _showCenteredSnackBar(context),
+      ),
+      _gridSnackBarItem(
+        context: context,
+        label: "Show Colored Success SnackBar",
+        onPressed: () => _showColoredSuccessSnackBar(context),
+      ),
+      _gridSnackBarItem(
+        context: context,
+        label: "Show Colored Error SnackBar",
+        onPressed: () => _showColoredErrorSnackBar(context),
+      ),
+      _gridSnackBarItem(
+        context: context,
+        label: "Show Colored Warning SnackBar",
+        onPressed: () => _showColoredWarningSnackBar(context),
+      ),
+      _gridSnackBarItem(
+        context: context,
+        label: "Show Colored Help SnackBar",
+        onPressed: () => _showColoredHelpSnackBar(context),
+      ),
+    ];
+  }
+
+  /// Helper method to create a Grid item for each SnackBar
+  Widget _gridSnackBarItem({
+    required BuildContext context,
+    required String label,
+    required VoidCallback onPressed,
+  }) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[50], // Background color
+          borderRadius: BorderRadius.circular(8.0), // Rounded corners
+          border: Border.all(color: Colors.blueAccent), // Border style
+        ),
+        child: Center(
+          child: Text(
+            label,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 14.0, fontWeight: FontWeight.w600),
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Displays a bordered success SnackBar
+  void _showSuccessSnackBar(BuildContext context) {
+    showProKitSnackBar(
+      context,
+      title: "Success",
+      message: "Operation completed successfully.",
+      snackBarType: ProKitSnackBarType.bordered,
+      notificationType: ProKitNotificationType.success,
+    );
+  }
+
+  /// Displays a bordered error SnackBar
+  void _showErrorSnackBar(BuildContext context) {
+    showProKitSnackBar(
+      context,
+      title: "Error",
+      message: "Something went wrong.",
+      snackBarType: ProKitSnackBarType.bordered,
+      notificationType: ProKitNotificationType.failure,
+      snackBarPosition: ProKitSnackBarPosition.topRight,
+    );
+  }
+
+  /// Displays a bordered warning SnackBar
+  void _showWarningSnackBar(BuildContext context) {
+    showProKitSnackBar(
+      context,
+      title: "Warning",
+      message: "This is a warning message.",
+      snackBarType: ProKitSnackBarType.bordered,
+      notificationType: ProKitNotificationType.warning,
+      color: Colors.deepOrange,
+      autoCloseDuration: const Duration(seconds: 5),
+    );
+  }
+
+  /// Displays a bordered help SnackBar
+  void _showHelpSnackBar(BuildContext context) {
+    showProKitSnackBar(
+      context,
+      title: "Help",
+      message: "Need help with something?",
+      snackBarType: ProKitSnackBarType.bordered,
+      notificationType: ProKitNotificationType.help,
+      customIcon: const Icon(Icons.help_outline, color: Colors.blue),
+      titleTextStyle: const TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: Colors.blue,
+      ),
+      messageTextStyle: const TextStyle(
+        fontSize: 14,
+        color: Colors.blue,
+      ),
+    );
+  }
+
+  /// Displays a bordered custom size SnackBar
+  void _showCustomSizeSnackBar(BuildContext context) {
+    showProKitSnackBar(
+      context,
+      title: "Custom Size",
+      message: "This SnackBar has a custom width and height.",
+      snackBarType: ProKitSnackBarType.bordered,
+      notificationType: ProKitNotificationType.success,
+      width: 300,
+      height: 100,
+    );
+  }
+
+  /// Displays a bordered centered SnackBar
+  void _showCenteredSnackBar(BuildContext context) {
+    showProKitSnackBar(
+      context,
+      title: "Centered SnackBar",
+      message: "This SnackBar is displayed in the center of the screen.",
+      snackBarType: ProKitSnackBarType.bordered,
+      notificationType: ProKitNotificationType.help,
+      autoClose: false,
+      snackBarPosition: ProKitSnackBarPosition.center,
+    );
+  }
+
+  /// Displays a colored success SnackBar
+  void _showColoredSuccessSnackBar(BuildContext context) {
+    showProKitSnackBar(
+      context,
+      title: "Success",
+      message: "Operation completed successfully.",
+      snackBarType: ProKitSnackBarType.colored,
+      notificationType: ProKitNotificationType.success,
+    );
+  }
+
+  /// Displays a colored error SnackBar
+  void _showColoredErrorSnackBar(BuildContext context) {
+    showProKitSnackBar(
+      context,
+      title: "Error",
+      message: "Something went wrong.",
+      snackBarType: ProKitSnackBarType.colored,
+      notificationType: ProKitNotificationType.failure,
+      snackBarPosition: ProKitSnackBarPosition.topRight,
+    );
+  }
+
+  /// Displays a colored warning SnackBar
+  void _showColoredWarningSnackBar(BuildContext context) {
+    showProKitSnackBar(
+      context,
+      title: "Warning",
+      message: "This is a warning message.",
+      snackBarType: ProKitSnackBarType.colored,
+      notificationType: ProKitNotificationType.warning,
+      color: Colors.deepOrange,
+      autoCloseDuration: const Duration(seconds: 5),
+    );
+  }
+
+  /// Displays a colored help SnackBar
+  void _showColoredHelpSnackBar(BuildContext context) {
+    showProKitSnackBar(
+      context,
+      title: "Help",
+      message: "Need help with something?",
+      snackBarType: ProKitSnackBarType.colored,
+      notificationType: ProKitNotificationType.help,
+      customIcon: const Icon(Icons.help_outline, color: Colors.white),
+      titleTextStyle: const TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+      ),
+      messageTextStyle: const TextStyle(
+        fontSize: 14,
+        color: Colors.white70,
+      ),
     );
   }
 }
